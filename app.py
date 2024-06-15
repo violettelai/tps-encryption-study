38459573f67cdab2d15ce351cd18844f5aa92f29
violettelai <xviolette02@gmail.com>
2024-06-15 14:42:52 +0800

    Add steps to do

--- app.py
+++ app.py
@@ -3,23 +3,38 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def fetchLoginTemplate():
     return render_template("login.html")
 
 @app.route("/login", methods=["POST"])
 def login():
-    data = request.json
+    data = request.get_json()
     username = data.get("username")
     password = data.get("password")
 
+    # Fetch from db
+    
+    # Decrypt in 3 ways
+
+    # Check valid credentials
     if username == "admin" and password == "admin":
         return "Welcome, admin!"
     else:
         return "Invalid credentials, please try again."
 
 @app.route("/register")
-def register():
+def fetchRegisterTemplate():
     return render_template("register.html")
 
+@app.route("/register", methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    
+    # Encrypt in 3 ways
+    
+    # Insert DB
+    
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
