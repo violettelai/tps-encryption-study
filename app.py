6409a735b9550fcac01598a47fbeb11f1164372d
evonnn16 <evonngyt1602@gmail.com>
2024-06-17 12:04:51 +0800

    basic db insert and fetch

--- app.py
+++ app.py
@@ -11,10 +11,8 @@ def fetchLoginTemplate():
 def init():
     db = mysql.connector.connect(host="localhost", user="root", password="")
     dbcursor = db.cursor()
-
     dbcursor.execute("CREATE DATABASE IF NOT EXISTS pysec")
-    # db = mysql.connector.connect(host="localhost", user="root", password="", database="pysec2")
-    
+    # db = mysql.connector.connect(host="localhost", user="root", password="", database="pysec2")    
     dbcursor.execute("USE pysec")
     # dbcursor = db.cursor()
     dbcursor.execute("CREATE TABLE IF NOT EXISTS user (username VARCHAR(255) PRIMARY KEY, name VARCHAR(255), pwd VARCHAR(255), rsakey BLOB)")
@@ -27,12 +25,19 @@ def login():
     password = data.get("password")
 
     # Fetch from db
-    
+    db = mysql.connector.connect(host="localhost", user="root", password="", database="pysec")
+    dbcursor = db.cursor()
+    sql = "SELECT * FROM user WHERE username = %s"
+    uname = (username, )
+    dbcursor.execute(sql, uname)
+    result = dbcursor.fetchone()
+    print(f"result: {result}")
+
     # Decrypt in 3 ways
 
     # Check valid credentials
-    if username == "admin" and password == "admin":
-        return "Welcome, admin!"
+    if password == result[2]:
+        return f"Welcome, {username}!"
     else:
         return "Invalid credentials, please try again."
 
@@ -47,14 +52,27 @@ def register():
     password = data.get("password")
     conpass = data.get("conpassword")
     
+    if password != conpass: return "Passwords are not the same!"
+    
     # Encrypt in 3 ways
     
-    # Insert DB
+    db = mysql.connector.connect(host="localhost", user="root", password="", database="pysec")
+    dbcursor = db.cursor()
+    
+    # Check if username duplicated
+    sql = "SELECT username FROM user WHERE username = %s"
+    uname = (username, )
+    dbcursor.execute(sql, uname)
+    result = dbcursor.fetchone()
 
-    if password != conpass:
-        return "Passwords are not the same!"
-    else: 
-        return "Registration successful!"
+    if(result == None):
+        # Insert DB
+        sql = "INSERT INTO user (username, pwd) VALUES (%s, %s)"
+        val = (username, password)
+        dbcursor.execute(sql, val)
+        db.commit()
+        return "Registered successfully!"
+    else: return "Username exists."
     
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
